dc718ee908c45d7fadf707bd85573f629a5f9727
pigleter <pigleter@gmail.com>
2017-01-23 18:03:59 +0800

    修改为按资源库执行作业

--- src/com/task/quartz/QuartzJob.java
+++ src/com/task/quartz/QuartzJob.java
@@ -2,8 +2,16 @@ package com.task.quartz;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
+import com.jfinal.kit.PropKit;
+
 import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.repository.Repository;
+import org.pentaho.di.repository.RepositoryDirectoryInterface;
+import org.pentaho.di.repository.kdr.KettleDatabaseRepository;
+import org.pentaho.di.repository.kdr.KettleDatabaseRepositoryMeta;
 import org.pentaho.di.job.Job;
 
 public class QuartzJob implements org.quartz.Job {
@@ -11,19 +19,28 @@ public class QuartzJob implements org.quartz.Job {
 	public void execute(JobExecutionContext args) throws JobExecutionException {
 		
 		try{
+			PropKit.use("config.properties");
 			String jobName = args.getJobDetail().getJobDataMap().getString("jobName");	
-			if(!jobName.substring(jobName.length() - 3, jobName.length()).toLowerCase().equals(".kjb")){
-				jobName = jobName + ".kjb";
-			}
-			String jobPath = "C:\\Users\\ivan.yu\\Desktop\\" + jobName;
+//			if(!jobName.substring(jobName.length() - 3, jobName.length()).toLowerCase().equals(".kjb")){
+//				jobName = jobName + ".kjb";
+//			}
+//			String jobPath = "C:\\Users\\ivan.yu\\Desktop\\" + jobName;
 			String params[] = null;
 			if(args.getJobDetail().getJobDataMap().getString("param") != null){
 				params = args.getJobDetail().getJobDataMap().getString("param").split(",");
 			}
 			
 			KettleEnvironment.init();
-			JobMeta jobMeta = new JobMeta(jobPath, null);
-			Job job = new Job(null, jobMeta);
+			KettleDatabaseRepository repository = new KettleDatabaseRepository();
+			DatabaseMeta dataMeta = new DatabaseMeta("kettle_dev_for_api", "Oracle", "Native", PropKit.get("ip"), "wmprd", "1521", PropKit.get("kettleUser"), PropKit.get("kettlePwd"));
+			KettleDatabaseRepositoryMeta kettleDatabaseMeta = new KettleDatabaseRepositoryMeta("kettle_dev_for_api", "kettle_dev_for_api", "kettle_dev_for_api", dataMeta);
+			repository.init(kettleDatabaseMeta);
+			repository.connect(PropKit.get("repositoryUser"), PropKit.get("repositoryPwd"));
+			RepositoryDirectoryInterface directory = repository.findDirectory("/");
+			JobMeta jobMeta = ((Repository)repository).loadJob(jobName, directory, null, null );
+			Job job = new Job(repository, jobMeta);
+//			JobMeta jobMeta = new JobMeta(jobPath, null);
+//			Job job = new Job(null, jobMeta);
 			if(args.getJobDetail().getJobDataMap().getString("param") != null){
 				for(int i = 0; i < params.length; i++){
 					job.getJobMeta().setParameterValue(params[i].split("=")[0], params[i].split("=")[1]);
