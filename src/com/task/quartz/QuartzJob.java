56cb543690d7e8371f61eddfd10a1c391f2de7eb
pigleter <pigleter@gmail.com>
2017-01-26 15:40:36 +0800

    更新kettle版本后可以使用数据库资源库

--- src/com/task/quartz/QuartzJob.java
+++ src/com/task/quartz/QuartzJob.java
@@ -5,13 +5,18 @@ import org.quartz.JobExecutionException;
 
 import com.jfinal.kit.PropKit;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
 import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.repository.Repository;
 import org.pentaho.di.repository.RepositoryDirectoryInterface;
-import org.pentaho.di.repository.filerep.KettleFileRepository;
-import org.pentaho.di.repository.filerep.KettleFileRepositoryMeta;
+import org.pentaho.di.repository.kdr.KettleDatabaseRepository;
+import org.pentaho.di.repository.kdr.KettleDatabaseRepositoryMeta;
 import org.pentaho.di.job.Job;
 
 public class QuartzJob implements org.quartz.Job {
@@ -22,37 +27,32 @@ public class QuartzJob implements org.quartz.Job {
 		
 		try{
 			PropKit.use("config.properties");
-			String jobName = args.getJobDetail().getJobDataMap().getString("jobName");	
-//			if(!jobName.substring(jobName.length() - 3, jobName.length()).toLowerCase().equals(".kjb")){
-//				jobName = jobName + ".kjb";
-//			}
-//			String jobPath = "C:\\Users\\ivan.yu\\Desktop\\" + jobName;
+			String jobName = args.getJobDetail().getJobDataMap().getString("jobName");
 			String params[] = null;
 			if(args.getJobDetail().getJobDataMap().getString("param") != null){
 				params = args.getJobDetail().getJobDataMap().getString("param").split(",");
 			}
 			
 			KettleEnvironment.init();
-			KettleFileRepositoryMeta repinfo = new KettleFileRepositoryMeta("KettleFileRepository","KettleFileRepository","KettleFileRepository","file:///C:/Users/ivan.yu/KettleFileRepository");
-			KettleFileRepository rep = new KettleFileRepository();
-			rep.init(repinfo);
-//			KettleDatabaseRepository repository = new KettleDatabaseRepository();
-//			DatabaseMeta dataMeta = new DatabaseMeta("kettle_dev_for_api", "Oracle", "Native", PropKit.get("ip"), "wmprd", "1521", PropKit.get("kettleUser"), PropKit.get("kettlePwd"));
-//			KettleDatabaseRepositoryMeta kettleDatabaseMeta = new KettleDatabaseRepositoryMeta("kettle_dev_for_api", "kettle_dev_for_api", "kettle_dev_for_api", dataMeta);
-//			repository.init(kettleDatabaseMeta);
-//			repository.connect(PropKit.get("repositoryUser"), PropKit.get("repositoryPwd"));
-			RepositoryDirectoryInterface directory = rep.findDirectory("/");
-//			JobMeta jobMeta = ((Repository)repository).loadJob(jobName, directory, null, null );
-			JobMeta jobMeta = ((Repository)rep).loadJob(jobName, directory, null, null);
-			Job job = new Job(rep, jobMeta);
-//			JobMeta jobMeta = new JobMeta(jobPath, null);
-//			Job job = new Job(null, jobMeta);
+			DatabaseMeta dataMeta = new DatabaseMeta("kettle_dev_for_api", "Oracle", "Native", PropKit.get("ip"), "wmprd", "1521", PropKit.get("kettleUser"), PropKit.get("kettlePwd"));
+			KettleDatabaseRepository repository = new KettleDatabaseRepository();			
+			KettleDatabaseRepositoryMeta kettleDatabaseMeta = new KettleDatabaseRepositoryMeta("kettle_dev_for_api", "kettle_dev_for_api", "kettle_dev_for_api", dataMeta);
+			repository.init(kettleDatabaseMeta);
+			repository.connect(PropKit.get("repositoryUser"), PropKit.get("repositoryPwd"));
+			RepositoryDirectoryInterface directory = repository.findDirectory("/IvanTest");
+			JobMeta jobMeta = ((Repository)repository).loadJob(jobName, directory, null, null );
+			Job job = new Job(repository, jobMeta);
 			if(args.getJobDetail().getJobDataMap().getString("param") != null){
 				for(int i = 0; i < params.length; i++){
 					job.getJobMeta().setParameterValue(params[i].split("=")[0], params[i].split("=")[1]);
 				}
 			}
 			job.start();
+			Date currentTime = new Date();
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+			String dateString = formatter.format(currentTime);
+			String uniqueJobID = dateString + String.valueOf((long)((Math.random()*9+1)*100000));
+			MDC.put("uniqueJobID", uniqueJobID);
 			logger.info("Kettle作业开始执行!");
 			job.waitUntilFinished();
 			logger.info("Kettle作业执行完成!");
